81d3a8c0a8ef6508987dd4d921889ee7c45bab8f
dshal <shalimov1360@gmail.com>
2016-07-09 15:21:08 +0300

    reduce high card check

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 var NAME = "Crazy Penguins";
 module.exports = {
 
-  VERSION: "0.0.10",
+  VERSION: "0.1.1",
 
   bet_request: function (game_state, bet) {
     var highCards = ['J', 'Q', 'K', 'A'];
@@ -11,16 +11,15 @@ module.exports = {
     var player = game_state.players[game_state.in_action];
 
     function getMinBetForKeepPlaying(){
-          return game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
-          }
-
-
+        return game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
+    }
+    
     function isCardNotEmpty(){
-      return player.hole_cards[0] && player.hole_cards[1];
+        return player.hole_cards[0] && player.hole_cards[1];
     }
 
     function isPairCards(){
-      return player.hole_cards[0].rank == player.hole_cards[1].rank;
+        return player.hole_cards[0].rank == player.hole_cards[1].rank;
     }
 
     function isHighCardPresent(){
@@ -33,27 +32,27 @@ module.exports = {
       // return smallCardsForPair.some(function (cardsElement) {
       //   return player.hole_cards[0].rank == cardsElement || player.hole_cards[1].rank == cardsElement;
       // });
-      return false;
+        return false;
     }
 
     var minBet = getMinBetForKeepPlaying();
 
     function isBetNotBig(){
-          return minBet < player.stack * betTreshold;
-          }
+        return minBet < player.stack * betTreshold;
+    }
 
     if (isCardNotEmpty() && isPairCards()) {
-      if (isSmallCardPresent()) {
-        //70% of all in!
-        bet(Math.round(player.stack * smallPairBetMultiplier));
-      } else {
-        //All in!
-        bet(player.stack);
-      }
+        if (isSmallCardPresent()) {
+          //70% of all in!
+            bet(Math.round(player.stack * smallPairBetMultiplier));
+        } else {
+            //All in!
+            bet(player.stack);
+          }
     } else if (/*isBetNotBig() &&*/ isCardNotEmpty() && isHighCardPresent()) {
-      bet(minBet + game_state.minimum_raise);
-    } else {
-      bet(0);
+    //     bet(minBet + game_state.minimum_raise);
+    // } else {
+        bet(0);
     }
   },
 
