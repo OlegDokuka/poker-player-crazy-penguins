1cf1efee4b219845a223b210a51cf2501f22bb8a
Shadowgun <shadowgun@i.ua>
2016-07-09 13:12:57 +0300

    fix code issue

--- player.js
+++ player.js
@@ -1,13 +1,13 @@
 var NAME = "Crazy Penguins";
 module.exports = {
 
-  VERSION: "0.0.4",
+  VERSION: "0.0.6",
 
   bet_request: function (game_state, bet) {
     var player = game_state.players[game_state.in_action];
     var maxbet = game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
 
-    if (player.hole_cards[0].rank == player.hole_cards[1].rank) {
+    if (player.hole_cards[0] && player.hole_cards[1] && player.hole_cards[0].rank == player.hole_cards[1].rank) {
       bet(maxbet + game_state.minimum_raise);
     } else {
       bet(game_state.minimum_raise);
