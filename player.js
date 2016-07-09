4c50a913c040bac200e66a51399eba0e7f2fd7ff
dshal <shalimov1360@gmail.com>
2016-07-09 13:49:26 +0300

    refactor

--- player.js
+++ player.js
@@ -5,14 +5,29 @@ module.exports = {
 
   bet_request: function (game_state, bet) {
     var player = game_state.players[game_state.in_action];
-    var maxbet = game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
 
-    if (player.hole_cards[0] && player.hole_cards[1] && player.hole_cards[0].rank == player.hole_cards[1].rank) {
+    function getMinBetForKeepPlaying(){
+          return game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
+          }
+
+
+    function isCardNotEmpty(){
+      return player.hole_cards[0] && player.hole_cards[1];
+    }
+
+    function isPairCards(){
+      return player.hole_cards[0].rank == player.hole_cards[1].rank;
+    }
+
+    var minBet = getMinBetForKeepPlaying();
+
+    if (isCardNotEmpty() && isPairCards()) {
+      //All in!
       bet(player.stack);
-    } else if (maxbet < player.stack / 10) {
-      bet(maxbet + game_state.minimum_raise);
+    } else if (minBet < player.stack / 10) {
+      bet(minBet + game_state.minimum_raise);
     } else {
-      bet(game_state.minimum_raise);
+      bet(0);
     }
   },
 
