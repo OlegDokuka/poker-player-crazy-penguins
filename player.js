1f701b4ea816392af783333c66bf7d3c77899995
Shadowgun <shadowgun@i.ua>
2016-07-09 13:39:58 +0300

    add bet strategy

--- player.js
+++ player.js
@@ -8,6 +8,8 @@ module.exports = {
     var maxbet = game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
 
     if (player.hole_cards[0] && player.hole_cards[1] && player.hole_cards[0].rank == player.hole_cards[1].rank) {
+      bet(player.stack);
+    } else if (maxbet < player.stack / 10) {
       bet(maxbet + game_state.minimum_raise);
     } else {
       bet(game_state.minimum_raise);
