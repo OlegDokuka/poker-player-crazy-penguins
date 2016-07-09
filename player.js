3b8db86e9a303a001c833e5a1d83e52602c70eca
Shadowgun <shadowgun@i.ua>
2016-07-09 12:59:20 +0300

    fix bug

--- player.js
+++ player.js
@@ -1,19 +1,17 @@
 var NAME = "Crazy Penguins";
 module.exports = {
 
-  VERSION: "0.0.5",
+  VERSION: "0.0.4",
 
   bet_request: function (game_state, bet) {
-    // var player = game_state.players[game_state]
-    // var maxbet = game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
+    var player = game_state.players[game_state.in_action];
+    var maxbet = game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
 
-    // if (player.hole_cards[0].rank == player.hole_cards[1]) {
-    //   bet(maxbet + game_state.minimum_raise);
-    // } else {
-    //   bet(game_state.minimum_raise);
-    // }
-
-    bet(game_state.minimum_raise);
+    if (player.hole_cards[0].rank == player.hole_cards[1].rank) {
+      bet(maxbet + game_state.minimum_raise);
+    } else {
+      bet(game_state.minimum_raise);
+    }
   },
 
   showdown: function (game_state) {
