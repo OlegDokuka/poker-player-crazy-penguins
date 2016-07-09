44229a082cbfd837ecf6b9ff12c93a1c721dc588
dshal <shalimov1360@gmail.com>
2016-07-09 16:40:19 +0300

    less player checking

--- player.js
+++ player.js
@@ -5,8 +5,12 @@ module.exports = {
 
   bet_request: function (game_state, bet) {
     var highCards = ['10', 'J', 'Q', 'K', 'A'];
+    var highCardsForLessPeople = ['7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
     var smallCardsForPair = ['2', '3', '4', '5'];
-    var betTreshold = 0.2; //10% of player money, see isBetNotBig() function
+    var betTreshold = 0.2; //prcentages of player money, see isBetNotBig() function
+    var playersTreshold = 3;
+    var allInStep = 0.25;
     var smallPairBetMultiplier = 0.7;
     var player = game_state.players[game_state.in_action];
 
@@ -22,10 +26,18 @@ module.exports = {
         return player.hole_cards[0].rank == player.hole_cards[1].rank;
     }
 
+    function isCardPresent(array) {
+      return array.some(function (cardsElement) {
+        return player.hole_cards[0].rank == cardsElement || player.hole_cards[1].rank == cardsElement;
+      });
+    }
+
     function isHighCardPresent(){
-      return highCards.some(function (cardsElement) {
-            return player.hole_cards[0].rank == cardsElement || player.hole_cards[1].rank == cardsElement;
-          });
+      return isCardPresent(highCards);
+    }
+
+    function isHighCardForLessPeoplePresent(){
+      return isCardPresent(highCardsForLessPeople);
     }
 
     function isSmallCardPresent(){
@@ -41,11 +53,31 @@ module.exports = {
         return minBet < player.stack * betTreshold;
     }
 
+    function getActivePlayersCount() {
+      return game_state.players.filter(function (p) { return p.status == "active" }).length;
+    }
+
+    function manyPlayersCanPlay() {
+        return getActivePlayersCount() > playersTreshold;
+    }
+
+    function getAllIn(){
+        // bet(player.stack);
+      bet(Math.round(minBet + (player.stack * allInStep)));
+    }
+
     if (isCardNotEmpty() && isPairCards() && isHighCardPresent()) {
       //All in!
-      bet(player.stack);
-    // } else if (/*isBetNotBig() &&*/ isCardNotEmpty() && isHighCardPresent()) {
-    //     bet(minBet + game_state.minimum_raise);
+      getAllIn();
+    } else if (!manyPlayersCanPlay()) {
+        if (isCardNotEmpty() && isPairCards() && isHighCardForLessPeoplePresent()) {
+          //All in!
+          getAllIn();
+        } else if (/*isBetNotBig() &&*/ isCardNotEmpty() && isHighCardPresent()) {
+            bet(minBet/* + game_state.minimum_raise*/);
+        } else {
+            bet(0);
+        }
     } else {
         bet(0);
     }
