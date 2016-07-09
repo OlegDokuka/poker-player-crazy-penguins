426c31d8692a832f543c1384c87349b586249958
dshal <shalimov1360@gmail.com>
2016-07-09 14:09:15 +0300

    add highCard check

--- player.js
+++ player.js
@@ -1,9 +1,10 @@
 var NAME = "Crazy Penguins";
 module.exports = {
 
-  VERSION: "0.0.7",
+  VERSION: "0.0.8",
 
   bet_request: function (game_state, bet) {
+    var highCards = ['10', 'J', 'Q', 'K', 'A'];
     var player = game_state.players[game_state.in_action];
 
     function getMinBetForKeepPlaying(){
@@ -19,12 +20,22 @@ module.exports = {
       return player.hole_cards[0].rank == player.hole_cards[1].rank;
     }
 
+    function isHighCardPresent(){
+      return highCards.some(function (cardsElement) {
+            return player.hole_cards[0].rank == cardsElement || player.hole_cards[1].rank == cardsElement;
+          });
+    }
+
     var minBet = getMinBetForKeepPlaying();
+
+    function isBetNotBig(){
+          return minBet < player.stack / 10;
+          }
 
     if (isCardNotEmpty() && isPairCards()) {
       //All in!
       bet(player.stack);
-    } else if (minBet < player.stack / 10) {
+    } else if (isBetNotBig() && isCardNotEmpty() && isHighCardPresent()) {
       bet(minBet + game_state.minimum_raise);
     } else {
       bet(0);
