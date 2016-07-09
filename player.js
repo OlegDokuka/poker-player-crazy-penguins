9f3f98d4affa0af987578b11e4ea6ac27883cc33
Shadowgun <shadowgun@i.ua>
2016-07-09 12:40:56 +0300

    add logic for min bet in round

--- player.js
+++ player.js
@@ -5,7 +5,8 @@ module.exports = {
 
   bet_request: function (game_state, bet) {
     //var player = game_state.players[]
-    bet(game_state.minimum_raise);
+    var maxbet = game_state.players.reduce(function (p, n) { return n.bet > p ? n.bet : p }, 0);
+    bet(maxbet + game_state.minimum_raise);
   },
 
   showdown: function (game_state) {
